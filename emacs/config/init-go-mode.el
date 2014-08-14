221f8fe809c9ca433bf497f069f9bffd1d77f509
Romano Augusto <romanoaugusto88@gmail.com>
2014-08-14 15:29:54 -0300

    Emacs: Go mode improvement on gofmt errors

--- emacs/config/init-go-mode.el
+++ emacs/config/init-go-mode.el
@@ -9,5 +9,9 @@
                           (company-mode)))
 (setq gofmt-command "goimports")
 (add-hook 'before-save-hook #'gofmt-before-save)
+; Clear the "Gofmt Errors" window after the code is fixed
+(add-hook 'after-save-hook (lambda ()
+                              (unless (get-buffer "*Gofmt Errors*")
+                                (delete-other-windows))))
 
 (provide 'init-go-mode)
