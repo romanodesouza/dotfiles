b60930654b3f2e32c6b0074880b22bad896e096f
Romano Augusto <romanoaugusto88@gmail.com>
2014-08-19 21:13:51 -0300

    Emacs: Adjustments on evil mode
    - <leader>s now, saves and goes to normal mode;
    - <leader>a goes to the first non-black character of the line;
    - <leader>e goes to the end of line;
    - <leader>g goes to previous buffer;
    - <leader>h goes to next buffer;
    - Setup a workaround to echo the <leader> character itself.

--- emacs/config/init-evil-mode.el
+++ emacs/config/init-evil-mode.el
@@ -2,24 +2,33 @@
 (require-package 'evil-leader)
 (require-package 'evil-nerd-commenter)
 
+; Setup evil leader
 (setq evil-leader/in-all-states t)
 (setq evil-leader/no-prefix-mode-rx '(".*-mode"))
-; Setup evil leader
 (global-evil-leader-mode)
 (evil-leader/set-leader ",")
 (evil-leader/set-key
   "f" 'fuzzy-finder
   "b" 'switch-to-buffer
   "k" 'kill-buffer
-  "s" 'save-buffer
+  "s" 'save-buffer-and-goes-to-normal-mode
   "q" 'quit-window
-  "n" 'new-buffer
   "i" 'imenu
+  "a" 'back-to-indentation
+  "e" 'end-of-line
+  "g" 'previous-buffer
+  "h" 'next-buffer
+  ; Workaround to print "comma"(the leader char):
+  "," (kbd "C-q ,")
 )
 ; Enable evil-nerd-commenter
 (evilnc-default-hotkeys)
 ; Enable evil mode
 (evil-mode t)
 
+(defun save-buffer-and-goes-to-normal-mode()
+  (interactive)
+  (save-buffer)
+  (evil-normal-state))
 
 (provide 'init-evil-mode)
