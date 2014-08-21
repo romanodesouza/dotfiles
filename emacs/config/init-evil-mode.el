de9b95065c079d0b905b79511a3f57d7fa8725aa
Romano Augusto <romanoaugusto88@gmail.com>
2014-08-21 15:16:04 -0300

    Emacs: Improvements of docs and navigation between the last two buffers.

--- emacs/config/init-evil-mode.el
+++ emacs/config/init-evil-mode.el
@@ -16,7 +16,7 @@
   "i" 'imenu
   "a" 'back-to-indentation
   "e" 'end-of-line
-  "g" 'previous-buffer
+  "g" 'switch-to-previous-buffer
   "h" 'next-buffer
   ; Workaround to print "comma"(the leader char):
   "," (kbd "C-q ,")
@@ -26,9 +26,16 @@
 ; Enable evil mode
 (evil-mode t)
 
-(defun save-buffer-and-goes-to-normal-mode()
+(defun save-buffer-and-goes-to-normal-mode ()
+  "Evil: Save buffer and goes to normal mode."
   (interactive)
   (save-buffer)
   (evil-normal-state))
+
+(defun switch-to-previous-buffer ()
+  "Switch to previously open buffer.
+Repeated invocations toggle between the two most recently open buffers."
+  (interactive)
+  (switch-to-buffer (other-buffer (current-buffer) 1)))
 
 (provide 'init-evil-mode)
