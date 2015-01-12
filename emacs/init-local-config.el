475ea99bdfacee338e6471089f18b4a08fc13ee5
Romano Augusto <romanoaugusto88@gmail.com>
2015-01-12 02:15:10 -0200

    A lot of stuff to improve typing

--- emacs/init-local-config.el
+++ emacs/init-local-config.el
@@ -6,37 +6,69 @@
 ; Key bindings
 (global-unset-key (kbd "C-S-p"))
 (global-unset-key (kbd "C-+"))
-(global-set-key (kbd "<f5>") 'revert-buffer-no-confirm)
-(global-set-key (kbd "C-=") 'er/expand-region)
-(global-set-key (kbd "C--") 'er/contract-region)
 
-(define-key evil-normal-state-map (kbd ";")     'evil-ex)
+(global-set-key                   (kbd "M-h")   nil)
+(global-set-key                   (kbd "M-j")   nil)
+(global-set-key                   (kbd "M-k")   nil)
+(global-set-key                   (kbd "M-l")   nil)
+(global-set-key                   (kbd "M-w")   nil)
+(global-set-key                   (kbd "M-b")   nil)
+(global-set-key                   (kbd "M-e")   nil)
+(global-set-key                   (kbd "M-f")   nil)
+(global-set-key                   (kbd "M-r")   nil)
+(global-set-key                   (kbd "C-j")   nil)
+(define-key evil-insert-state-map (kbd "C-j")   nil)
+
+(global-set-key (kbd "<f5>")         'revert-buffer-no-confirm)
+(global-set-key (kbd "C-=")          'er/expand-region)
+(global-set-key (kbd "C--")          'er/contract-region)
+(global-set-key (kbd "<C-return>")   'open-line-below)
+(global-set-key (kbd "<C-S-return>") 'open-line-above)
+
 (define-key evil-normal-state-map (kbd "w")     'forward-word)
 (define-key evil-normal-state-map (kbd "b")     'backward-word)
-(define-key evil-normal-state-map (kbd "U")     'undo-tree-redo)
 (define-key evil-normal-state-map (kbd "/")     'helm-swoop)
-(define-key evil-normal-state-map (kbd "C-r")   'helm-semantic-or-imenu)
-(define-key evil-insert-state-map (kbd "C-r")   'helm-semantic-or-imenu)
-(define-key evil-normal-state-map (kbd "C-l")   'evil-repeat-find-char)
-(define-key evil-normal-state-map (kbd "C-h")   'evil-repeat-find-char-reverse)
-(define-key evil-insert-state-map (kbd "C-SPC") 'evil-delete-backward-word)
-(define-key evil-insert-state-map (kbd "C-d")   'kill-word)
-(define-key evil-normal-state-map (kbd "C-j")   'evil-visual-line-down)
-(define-key evil-normal-state-map (kbd "C-k")   'evil-visual-line-up)
-(define-key evil-normal-state-map (kbd "C-d")   'gcm-scroll-down)
-(define-key evil-normal-state-map (kbd "C-u")   'gcm-scroll-up)
+(define-key evil-normal-state-map (kbd "C-h")   'evil-window-left)
+(define-key evil-normal-state-map (kbd "C-j")   'evil-window-down)
+(define-key evil-normal-state-map (kbd "C-k")   'evil-window-up)
+(define-key evil-normal-state-map (kbd "C-l")   'evil-window-right)
 (define-key evil-normal-state-map (kbd "C-S-l") 'evil-window-move-far-right)
 (define-key evil-normal-state-map (kbd "C-S-h") 'evil-window-move-far-left)
-(define-key evil-insert-state-map (kbd "C-{")   'evil-normal-state)
 (define-key evil-normal-state-map (kbd "C-~")   'evil-first-non-blank)
 
-(key-chord-define evil-insert-state-map "oo" 'evil-open-below)
-(key-chord-define evil-insert-state-map "OO" 'evil-open-above)
+; Try to simulate some motions by Terminal's Alt escape key
+(define-key evil-insert-state-map (kbd "M-h")   'escape-backward-char)
+(define-key evil-insert-state-map (kbd "M-j")   'escape-next-line)
+(define-key evil-insert-state-map (kbd "M-k")   'escape-previous-line)
+(define-key evil-insert-state-map (kbd "M-l")   'escape-forward-char)
+(define-key evil-insert-state-map (kbd "M-w")   'escape-forward-word)
+(define-key evil-insert-state-map (kbd "M-b")   'escape-backward-word)
+(define-key evil-insert-state-map (kbd "M-e")   'escape-forward-WORD-end)
+(define-key evil-insert-state-map (kbd "M-f")   'evil-normal-state)
+
+(define-key evil-normal-state-map (kbd "M-h")   'evil-backward-char)
+(define-key evil-normal-state-map (kbd "M-j")   'evil-next-line)
+(define-key evil-normal-state-map (kbd "M-k")   'evil-previous-line)
+(define-key evil-normal-state-map (kbd "M-l")   'evil-forward-char)
+(define-key evil-normal-state-map (kbd "M-w")   'forward-word)
+(define-key evil-normal-state-map (kbd "M-b")   'backward-word)
+(define-key evil-normal-state-map (kbd "M-e")   'evil-forward-WORD-end)
+(define-key evil-normal-state-map (kbd "M-f")   'evil-find-char)
+
+; Key chord
+(key-chord-define-global ",w" 'delete-other-windows)
 (key-chord-define-global ",a" 'ag-project-regexp)
+(key-chord-define-global ".s" 'save-buffer-and-goes-to-normal-mode) ; typo
+
+(define-key evil-visual-state-map (kbd "y") 'my-evil-yank)
+
+; PHP mode
+(require-package 'php-mode)
 
 ; Jinja2 mode
 (require-package 'jinja2-mode)
 (add-to-list 'auto-mode-alist '("\\.html" . jinja2-mode))
+(add-to-list 'auto-mode-alist '("\\.volt" . jinja2-mode))
 (add-hook 'sgml-mode-hook (lambda ()
                             (key-chord-define sgml-mode-map "hd" 'sgml-delete-tag)
                             (key-chord-define sgml-mode-map "hf" 'sgml-skip-tag-forward)
@@ -58,28 +90,63 @@
       (error "Nothing to close")))
   (save-excursion (jinja2-indent-line)))
 
-; Scroll down 1 line
-(defun gcm-scroll-down ()
-  (interactive)
-  (scroll-up 1))
-
-; Scroll up 1 line
-(defun gcm-scroll-up ()
-  (interactive)
-  (scroll-down 1))
-
-(defun evil-visual-line-down ()
-  (interactive)
-  (evil-visual-line)
-  (evil-next-line))
-
-(defun evil-visual-line-up ()
-  (interactive)
-  (evil-visual-line)
-  (evil-previous-line))
-
 (defun revert-buffer-no-confirm ()
   "Revert buffer without confirmation."
   (interactive) (revert-buffer t t))
+
+; Try to simulate the "vmap y ygv<Esc>" conf
+(evil-define-operator my-evil-yank (beg end type register yank-handler)
+                      (interactive "<R><x><y>")
+                      (evil-yank beg end type register yank-handler)
+                      (run-at-time "000 msec" nil 'evil-visual-restore)
+                      (run-at-time "001 msec" nil 'keyboard-quit))
+
+(defun open-line-below ()
+  (interactive)
+  (end-of-line)
+  (newline)
+  (indent-for-tab-command))
+
+(defun open-line-above ()
+  (interactive)
+  (beginning-of-line)
+  (newline)
+  (forward-line -1)
+  (indent-for-tab-command))
+
+(defun escape-backward-char ()
+  (interactive)
+  (evil-normal-state)
+  (evil-backward-char))
+
+(defun escape-next-line ()
+  (interactive)
+  (evil-normal-state)
+  (evil-next-line))
+
+(defun escape-previous-line ()
+  (interactive)
+  (evil-normal-state)
+  (evil-previous-line))
+
+(defun escape-forward-char ()
+  (interactive)
+  (evil-normal-state)
+  (evil-forward-char))
+
+(defun escape-forward-word ()
+  (interactive)
+  (evil-normal-state)
+  (forward-word))
+
+(defun escape-backward-word ()
+  (interactive)
+  (evil-normal-state)
+  (backward-word))
+
+(defun escape-forward-WORD-end ()
+  (interactive)
+  (evil-normal-state)
+  (evil-forward-WORD-end))
 
 (provide 'init-local-config)
