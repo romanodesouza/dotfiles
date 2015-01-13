7f136dcb66d3bbf559c149b1b8a4247ce7b92dba
Romano Augusto <romanoaugusto88@gmail.com>
2015-01-13 15:00:43 -0200

    Emacs: Go Oracle mode

--- emacs/init-local-config.el
+++ emacs/init-local-config.el
@@ -3,6 +3,29 @@
 (load-theme 'seti t)
 (set-default-font "DejaVu Sans Mono-14")
 
+; Go Oracle
+(add-hook 'go-mode-hook (lambda ()
+                          (load-file "~/.go/src/code.google.com/p/go.tools/cmd/oracle/oracle.el")
+                          (go-oracle-mode)))
+
+; PHP mode
+(require-package 'php-mode)
+
+; Jinja2 mode
+(require-package 'jinja2-mode)
+(add-to-list 'auto-mode-alist '("\\.html" . jinja2-mode))
+(add-to-list 'auto-mode-alist '("\\.volt" . jinja2-mode))
+(add-hook 'sgml-mode-hook (lambda ()
+                            (key-chord-define sgml-mode-map "hd" 'sgml-delete-tag)
+                            (key-chord-define sgml-mode-map "hf" 'sgml-skip-tag-forward)
+                            (key-chord-define sgml-mode-map "hb" 'sgml-skip-tag-backward)
+                            ; TODO: Change "//" by "</"
+                            (key-chord-define sgml-mode-map "//" 'sgml-close-tag)
+                            (key-chord-define jinja2-mode-map "jo" 'jinja2-insert-tag)
+                            (key-chord-define jinja2-mode-map "jc" 'my-jinja2-close-tag)
+                            (key-chord-define jinja2-mode-map "jv" 'jinja2-var-tag)))
+
+
 ; Key bindings
 (global-unset-key (kbd "C-S-p"))
 (global-unset-key (kbd "C-+"))
@@ -63,22 +86,7 @@
 
 (define-key evil-visual-state-map (kbd "y") 'my-evil-yank)
 
-; PHP mode
-(require-package 'php-mode)
-
-; Jinja2 mode
-(require-package 'jinja2-mode)
-(add-to-list 'auto-mode-alist '("\\.html" . jinja2-mode))
-(add-to-list 'auto-mode-alist '("\\.volt" . jinja2-mode))
-(add-hook 'sgml-mode-hook (lambda ()
-                            (key-chord-define sgml-mode-map "hd" 'sgml-delete-tag)
-                            (key-chord-define sgml-mode-map "hf" 'sgml-skip-tag-forward)
-                            (key-chord-define sgml-mode-map "hb" 'sgml-skip-tag-backward)
-                            ; TODO: Change "//" by "</"
-                            (key-chord-define sgml-mode-map "//" 'sgml-close-tag)
-                            (key-chord-define jinja2-mode-map "jo" 'jinja2-insert-tag)
-                            (key-chord-define jinja2-mode-map "jc" 'my-jinja2-close-tag)
-                            (key-chord-define jinja2-mode-map "jv" 'jinja2-var-tag)))
+;; Functions
 
 (defun my-jinja2-close-tag ()
   "Close the previously opened template tag."
