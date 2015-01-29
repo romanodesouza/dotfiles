ca427c864a057b321907fadb835006667f933eaf
Romano Augusto <romanoaugusto88@gmail.com>
2015-01-29 09:27:10 -0200

    Better key bindings

--- emacs/init-local-config.el
+++ emacs/init-local-config.el
@@ -37,26 +37,21 @@
 (global-set-key (kbd "C-<SPC>") nil)
 (define-key evil-insert-state-map (kbd "C-<SPC>") 'evil-normal-state)
 
-(global-set-key (kbd "C-j") nil)
-(define-key evil-insert-state-map (kbd "C-j") nil)
-
 (define-key evil-normal-state-map (kbd "w")     'forward-word)
 (define-key evil-normal-state-map (kbd "b")     'backward-word)
-
 (define-key evil-normal-state-map (kbd "C-h")   'evil-window-left)
 (define-key evil-normal-state-map (kbd "C-j")   'evil-window-down)
 (define-key evil-normal-state-map (kbd "C-k")   'evil-window-up)
 (define-key evil-normal-state-map (kbd "C-l")   'evil-window-right)
 (define-key evil-normal-state-map (kbd "C-S-l") 'evil-window-move-far-right)
 (define-key evil-normal-state-map (kbd "C-S-h") 'evil-window-move-far-left)
-
 (define-key evil-normal-state-map (kbd "C-~")   'evil-first-non-blank)
 
 ; Use Emacs search without regex
-(evil-define-key 'motion global-map (kbd ",f") 'isearch-forward)
-(define-key isearch-mode-map "\C-n" 'isearch-repeat-forward)
-(define-key isearch-mode-map "\C-p" 'isearch-repeat-backward)
-(define-key evil-motion-state-map (kbd "/") 'isearch-forward)
+(global-set-key (kbd "C-f") 'isearch-forward)
+(define-key evil-motion-state-map (kbd "C-f") 'isearch-forward)
+(define-key isearch-mode-map "\C-f" 'isearch-repeat-forward)
+(define-key evil-motion-state-map (kbd "/") 'helm-swoop)
 (define-key evil-motion-state-map (kbd "?") 'isearch-backward)
 (define-key evil-motion-state-map (kbd "n") 'isearch-repeat-forward)
 (define-key evil-motion-state-map (kbd "N") 'isearch-repeat-backward)
@@ -70,13 +65,5 @@
 
 ; Transpose words
 (key-chord-define-global ",t" 'transpose-words)
-; Helm swoop
-(key-chord-define-global ",d" 'helm-swoop)
-; imenu
-(key-chord-define-global ",r" 'helm-semantic-or-imenu)
-; visual line
-(key-chord-define-global ",v" 'evil-visual-line)
-; vertical split
-(evil-define-key 'normal global-map (kbd ",vs") 'evil-window-vsplit)
 
 (provide 'init-local-config)
