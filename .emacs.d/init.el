198a103a30a60134c371d885b4e39a28dbc02bdb
Romano Augusto <romanoaugusto88@gmail.com>
2018-01-02 23:48:08 +0000

    Emacs: Brand new setup for Go

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,66 +1,74 @@
 ;;; init.el --- My Emacs config
-;;; Author: Romano Augusto
-;;; License: MIT
-;;
-;; Garbage collection
-(setq gc-cons-threshold 50000000)
-;; TLS prime bits
-(setq gnutls-min-prime-bits 4096)
+;; Code:
+(setq
+ ;; Garbage collection
+ gc-cons-threshold 50000000
+ ;; TLS prime bits
+ gnutls-min-prime-bits 4096
+ ;; Disable backup
+ make-backup-files nil
+ ;; Disable lockfiles
+ create-lockfiles nil
+ ;; Disable auto saving
+ auto-save-default nil
+ ;; Always follow vc symlinks
+ vc-follow-symlinks t
+ ;; Don't show me the startup screen
+ inhibit-startup-screen t
+ ;; Empty initial message
+ initial-scratch-message ""
+ ;; Show column number in mode line
+ column-number-mode t
+ ;; Don't ring at error
+ ring-bell-function 'ignore
+ visible-bell 'top-bottom
+ ;; paren delay
+ show-paren-delay 0
+ ;; Uniquify style
+ uniquify-buffer-name-style 'forward
+ ;; Compiliation-mode settings
+ compilation-scroll-output 'first-error
+ compilation-always-kill t
+ compilation-error-regexp-alist nil
+ compilation-error-regexp-alist-alist nil
+ ;; Open buffers always on bottom
+ split-height-threshold nil
+ split-width-threshold most-positive-fixnum
+ ;; Safe local variables
+ enable-local-variables ':all)
+
+(setq-default
+ ;; Default indentation
+ tab-width 4
+ indent-tabs-mode nil
+ tab-always-indent 'complete
+ ;; Show full path of file in title bar
+ frame-title-format "%b (%f)"
+ ;; imenu
+ imenu-auto-rescan t
+ ;; Line spacing
+ line-spacing 3)
+
 ;; Enable deletion of selected text
 (delete-selection-mode t)
-;; Disable backup
-(setq make-backup-files nil)
-;; Disable lockfiles
-(setq create-lockfiles nil)
-;; Disable auto saving
-(setq auto-save-default nil)
-;; Always follow vc symlinks
-(setq vc-follow-symlinks t)
 ;; Reload file disk changed automatically
 (global-auto-revert-mode t)
-;; Default indentation
-(setq-default tab-width 4)
-(setq-default indent-tabs-mode nil)
-(setq-default tab-always-indent 'complete)
-;; Show full path of file in title bar
-(setq-default frame-title-format "%b (%f)")
-;; Open buffers always on bottom
-(setq split-height-threshold nil)
-(setq split-width-threshold most-positive-fixnum)
-;; Don't show me the startup screen
-(setq inhibit-startup-screen t)
-;; Empty initial message
-(setq initial-scratch-message "")
-;; Show column number in mode line
-(setq column-number-mode t)
-;; Don't ring at error
-(setq ring-bell-function 'ignore)
-(setq visible-bell 'top-bottom)
 ;; Disable menu bar
 (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 ;; Disable tool bar
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 ;; Disable scroll bar
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
-;; Aliases
+;; Shorter yes or no
 (defalias 'yes-or-no-p 'y-or-n-p)
-;; imenu
-(setq-default imenu-auto-rescan t)
 ;; Parens mode
 (show-paren-mode t)
-(setq show-paren-delay 0)
-;; Uniquify style
-(setq uniquify-buffer-name-style 'forward)
-;; Strip trailing whitespaces
+;; Strip trailing whitespaces before saving
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
-;; Line spacing
-(setq-default line-spacing 3)
-;; Font
-(defvar my/font "Liberation Mono 12")
-(set-face-attribute 'default nil :font my/font)
-(set-frame-font my/font nil t)
-;; init after init :P
-(add-hook 'after-init-hook 'my/init)
+                                        ; Font
+(let ((font "Liberation Mono 12"))
+  (set-face-attribute 'default nil :font font)
+  (set-frame-font font nil t))
 
 ;; Packages
 (require 'package)
@@ -90,22 +98,10 @@
 (el-get-bundle fzf
   :url "git@github.com:romanoaugusto88/fzf.el.git"
   :features fzf
-  (setq fzf/args "-x --margin 1,0")
-  (add-hook 'term-mode-hook 'my-term-mode-hook)
-  (defun my-term-mode-hook ()
-    ;; https://debbugs.gnu.org/cgi/bugreport.cgi?bug=20611
-    (setq bidi-paragraph-direction 'left-to-right)))
+  (setq fzf/args "-x --color bw --margin 0,1,1,0"))
 
-;;(use-package darcula-theme)
-(use-package dracula-theme
-  :config (load-theme 'dracula t))
-
-(use-package powerline
-  :init (powerline-default-theme))
-
-(use-package smooth-scrolling
-  :init (add-hook 'after-init-hook 'smooth-scrolling-mode)
-  :config (setq smooth-scroll-margin 10))
+(use-package color-theme-solarized
+  :config (load-theme 'solarized t))
 
 (use-package saveplace
   :init
@@ -113,21 +109,37 @@
   (setq save-place-forget-unreadable-files t
         save-place-skip-check-regexp "\\`/\\(?:cdrom\\|floppy\\|mnt\\|/[0-9]\\|\\(?:[^@/:]*@\\)?[^@/:]*[^@/:.]:\\)"))
 
-(use-package smex
-  :init (smex-initialize)
-  :bind ("M-x" . smex))
+(use-package smooth-scrolling
+  :init (add-hook 'after-init-hook 'smooth-scrolling-mode)
+  :config (setq smooth-scroll-margin 10))
 
 (use-package which-key
   :init
   (setq which-key-idle-delay 0.5)
   (add-hook 'after-init-hook 'which-key-mode))
 
-(use-package simpleclip)
+(use-package ivy
+  :commands (ivy-resume ivy-switch-buffer))
+
+(use-package counsel
+  :commands (counsel-ag counsel-imenu)
+  :bind ("M-x" . counsel-M-x))
 
 (use-package evil
   :init (setq evil-magic 'very-magic
               evil-want-C-u-scroll t)
   :config
+  ;; enable evil compatibility on compilation-mode
+  (add-hook 'compilation-mode-hook
+            (lambda()
+              (local-unset-key (kbd "g"))
+              (local-unset-key (kbd "h"))
+              (local-unset-key (kbd "-"))
+              (local-unset-key (kbd "?"))
+              (local-unset-key (kbd "0"))))
+
+  ;; goes to normal mode after saving bufers
+  (advice-add 'my/save-buffers :after #'evil-normal-state)
   (evil-mode t)
   (evil-set-initial-state 'term-mode 'emacs)
   (define-key evil-normal-state-map (kbd "C-l") 'evil-window-right)
@@ -148,67 +160,44 @@
     (fset 'evil-visual-update-x-selection 'ignore)
     (evil-leader/set-leader "<SPC>")
     (evil-leader/set-key
-     "o" 'fzf
-     "r" 'evil-search-backward
-     "s" 'evil-search-forward
-     "en" 'my/next-error))
+      "o" 'fzf
+      "r" 'evil-search-backward
+      "s" 'evil-search-forward
+      "en" 'my/next-error))
 
   (use-package evil-visualstar
     :config (global-evil-visualstar-mode t))
+
   (use-package evil-matchit
     :config (global-evil-matchit-mode t)))
+
+;;(use-package smex
+;;  :init (add-hook 'after-init-hook 'smex-initialize)
+;;  :bind ("M-x" . smex))
+
+(use-package expand-region
+  :commands (er/expand-region er/contract-region)
+  :init
+  (define-key evil-visual-state-map (kbd "v") 'er/expand-region)
+  (define-key evil-visual-state-map (kbd "V") 'er/contract-region))
 
 (use-package key-chord
   :init (add-hook 'after-init-hook (lambda () (key-chord-mode t)))
   :config
   (use-package key-seq
     :config
-    (key-seq-define-global ",b" 'ido-switch-buffer)
-    (key-seq-define-global ",s" 'my/save-buffers-and-goes-to-normal-mode)
+    (key-seq-define-global ",b" 'ivy-switch-buffer)
+    (key-seq-define-global ",s" 'my/save-buffers)
     (key-seq-define-global ",v" 'split-window-horizontally)
     (key-seq-define-global ",q" 'my/delete-window-maybe-kill-buffer)
     (key-seq-define-global ",w" 'delete-other-windows)
     (key-seq-define-global ",c" 'comment-dwim)
-    (key-seq-define-global ",d" 'idomenu)
-    (key-seq-define-global ",a" 'fzf-git-grep)
+    (key-seq-define-global ",d" 'counsel-imenu)
     (key-seq-define-global ",e" 'end-of-line)
     (key-seq-define-global "fd" 'evil-normal-state)
+    (key-seq-define-global ",a" 'fzf-git-grep)
 
     (add-hook 'minibuffer-setup-hook #'my/disable-key-chord-mode)))
-
-(use-package ido
-  :commands ido-switch-buffer
-  :init (setq ido-enable-flex-matching t
-              ido-ignore-extensions t
-              ido-use-virtual-buffers nil
-              ido-ignore-buffers '("\\` " "^\*")
-              ido-everywhere t)
-  :config
-  (ido-mode t)
-  (ido-everywhere t)
-
-  (use-package flx-ido
-    :init (setq ido-enable-flex-matching t
-                ido-use-faces nil)
-    :config (flx-ido-mode t))
-
-  (use-package ido-vertical-mode
-    :init (setq ido-vertical-define-keys 'C-n-C-p-up-and-down)
-    :config  (ido-vertical-mode t))
-
-  (use-package ido-ubiquitous
-    :config (ido-ubiquitous-mode t))
-
-  (use-package idomenu
-    :commands idomenu))
-
-(use-package expand-region
-  :commands (er/expand-region)
-  :init (define-key evil-visual-state-map (kbd "v") 'er/expand-region))
-
-(use-package multiple-cursors
-  :bind ("M-n" . mc/mark-more-like-this-extended)
-  :config (define-key mc/mark-more-like-this-extended-keymap (kbd "M-n") 'mc/mmlte--down))
 
 (use-package company
   :commands (global-company-mode)
@@ -217,9 +206,14 @@
   (setq company-idle-delay 0.1
         company-minimum-prefix-length 1
         company-tooltip-limit 20
+        company-tooltip-align-annotations t
         company-echo-delay 0
+        company-dabbrev-downcase nil
+        company-dabbrev-ignore-case t
+        company-global-modes '(not gud-mode)
         company-begin-commands '(self-insert-command)
-        company-backends '(company-elisp company-files))
+        company-transformers '(company-sort-by-backend-importance)
+        company-backends '(company-elisp company-files company-dabbrev))
   (define-key company-active-map (kbd "C-n") 'company-select-next)
   (define-key company-active-map (kbd "C-p") 'company-select-previous))
 
@@ -228,155 +222,44 @@
   :init (add-hook 'prog-mode-hook 'yas-global-mode)
   :config (setq yas-snippet-dirs (remq 'yas-installed-snippets-dir yas-snippet-dirs)))
 
-(use-package smartparens
-  :commands (smartparens-mode)
-  :init (add-hook 'prog-mode-hook 'smartparens-mode)
-  :config (sp-local-pair 'prog-mode "{" nil :post-handlers '((my/create-newline-and-enter-sexp "RET"))))
-
-(use-package project-explorer
-  :commands (project-explorer-open project-explorer-toggle)
-  :init
-  (setq pe/project-root-function 'projectile-project-root
-        pe/width 30
-        pe/follow-current t)
-  (evil-leader/set-key
-    "x" 'project-explorer-toggle)
-  :config
-  (evil-define-key 'normal project-explorer-mode-map (kbd "o") 'pe/return)
-  (evil-define-key 'normal project-explorer-mode-map (kbd "c") 'pe/create-file)
-  (evil-define-key 'normal project-explorer-mode-map (kbd "d") 'pe/delete-file))
-
-(use-package git-timemachine
-  :commands (git-timemachine)
-  :init
-  (add-hook 'git-timemachine-mode-hook #'evil-normalize-keymaps)
-  (evil-leader/set-key
-    "gt" 'git-timemachine)
-  :config
-  (evil-define-key 'normal git-timemachine-mode-map (kbd "p") 'git-timemachine-show-previous-revision)
-  (evil-define-key 'normal git-timemachine-mode-map (kbd "n") 'git-timemachine-show-next-revision)
-  (evil-define-key 'normal git-timemachine-mode-map (kbd "q") 'git-timemachine-quit))
-
-(use-package editorconfig
-  :config (editorconfig-mode t))
-
-;; Programming modes
-;; Go
+;; programming modes
 (use-package go-mode
   :mode "\\.go$"
   :init
-  (setq company-go-insert-arguments t
+  (setq company-go-show-annotation t
+        company-go-insert-arguments t
         gofmt-command "goimports")
   (add-hook 'before-save-hook 'gofmt-before-save)
   (setq go-mode-hook (lambda ()
                        (evil-define-key 'normal go-mode-map (kbd "C-]") 'godef-jump)
                        (evil-define-key 'normal go-mode-map (kbd "K") 'godef-describe)
-                       (setq imenu-generic-expression '((nil "func *\\(.*\\) {" 1)))
                        (go-eldoc-setup)
-                       (set (make-local-variable 'company-backends) '((company-files company-go company-yasnippet)))))
+                       (setq compilation-error-regexp-alist (delete 'go-test compilation-error-regexp-alist))
+                       (setq compilation-error-regexp-alist-alist (delete 'go-test compilation-error-regexp-alist-alist))
+                       (let ((name 'go)
+                             (pattern (concat "^\t+\\(" (getenv "GOPATH") "/src/[^:]+\\):\\([0-9]+\\)")))
+                         (add-to-list 'compilation-error-regexp-alist name)
+                         (add-to-list 'compilation-error-regexp-alist-alist (list name pattern 1 2) t))
+                       (set (make-local-variable 'company-backends) '((company-go :with company-yasnippet company-dabbrev)))))
+  ;; recompile after saving
+  (advice-add 'my/save-buffers :after (lambda () (when (derived-mode-p 'go-mode) (recompile))))
   :config
-  (use-package company-go)
+  (use-package company-go
+    :init (setq company-go-insert-arguments nil))
+
   (use-package go-eldoc))
 
-;; Javascript
-(use-package js2-mode
-  :mode "\\.jsx?$"
-  :config
-  (setq js-indent-level 2
-        js2-strict-missing-semi-warning nil
-        js2-strict-trailing-comma-warning nil)
-  (setq-default js2-basic-offset 2)
-  (use-package eslint-fix)
-  (add-hook 'js2-mode-hook (lambda ()
-                             (js2-imenu-extras-mode)
-                             (add-hook 'after-save-hook 'eslint-fix nil t)
-                             (add-hook 'company-completion-finished-hook #'(lambda (candidate)
-                                                                             (when (derived-mode-p 'js2-mode)
-                                                                               (when (string-suffix-p ".js" candidate)
-                                                                                 (delete-backward-char 3)))))
-                             (set (make-local-variable 'company-backends) '((company-files company-yasnippet))))))
-
-;; React
-(use-package rjsx-mode
-  :commands (rjsx-mode)
-  :init (add-to-list 'magic-mode-alist '("\\(.\\|\n\\)*import React" . rjsx-mode))
-  :config
-  (add-hook 'rjsx-mode-hook (lambda ()
-                              (evil-define-key 'insert rjsx-mode-map (kbd "C-d") 'rjsx-delete-creates-full-tag)
-                              (yas-activate-extra-mode 'js2-mode)
-                              (use-package emmet-mode
-                                :init
-                                (setq-local emmet-expand-jsx-className? t)
-                                (emmet-mode)
-                                (local-set-key (kbd "TAB") 'emmet-expand-yas)))))
-
-;; Elixir
-(use-package alchemist
-  :mode "\\.exs$"
-  :config
-  (elixir-mode)
-  (alchemist-mode))
-
-;; Ruby/Rails
-(add-hook 'ruby-mode-hook (lambda()
-                            (use-package inf-ruby
-                              :config
-                              (inf-ruby-minor-mode)
-                              (add-hook 'compilation-filter-hook 'inf-ruby-auto-enter))
-
-                            (use-package robe
-                              :config
-                              (robe-mode)
-                              (set (make-local-variable 'company-backends) '((company-files company-robe company-yasnippet)))
-                              (evil-define-key 'normal robe-mode-map (kbd "C-]") 'robe-jump)
-                              (evil-define-key 'normal robe-mode-map (kbd "K") 'robe-eldoc))
-
-                            (use-package rinari
-                              :config
-                              (rinari-minor-mode)
-                              (dolist (mode '(ruby-mode html-mode))
-                                            (evil-leader/set-key-for-mode mode
-                                              "fr" 'rinari-find-routes
-                                              "fh" 'rinari-find-helper
-                                              "fc" 'rinari-find-controller
-                                              "fm" 'rinari-find-model
-                                              "fd" 'rinari-find-migration
-                                              "fv" 'rinari-find-view)))))
-
-;; Programming related modes
-(use-package yaml-mode
-  :mode "\\.ya?ml$")
-
-(use-package dockerfile-mode
-  :mode "Dockerfile")
-
-(use-package nginx-mode
-  :commands (nginx-mode)
-  :init (add-to-list 'auto-mode-alist '("/nginx" . nginx-mode)))
-
-(use-package crontab-mode
-  :commands (crontab-mode)
-  :init (add-to-list 'auto-mode-alist '("/cron" . crontab-mode)))
-
-;; Functions
-(defun my/init ()
-  (my/key-bindings)
-  ;; Enable mouse when running in terminal
-  (unless (display-graphic-p)
-    (xterm-mouse-mode)))
-
-(defun my/key-bindings ()
-  ;; C-h erases previous character
-  (global-set-key (kbd "C-h") 'delete-backward-char)
-  (define-key isearch-mode-map (kbd "C-h") 'isearch-del-char)
-  (global-set-key (kbd "s-k") 'evil-scroll-line-up)
-  (global-set-key (kbd "s-j") 'evil-scroll-line-down))
-
-(defun my/save-buffers-and-goes-to-normal-mode ()
+;; functions
+(defun my/save-buffers ()
   (interactive)
   (save-some-buffers 'no-confirm)
-  (message "buffers has been saved")
-  (evil-normal-state))
+  (message "buffers has been saved"))
+
+(defun my/delete-window-maybe-kill-buffer ()
+  (interactive)
+  (when (eq 1 (length (get-buffer-window-list)))
+    (kill-this-buffer))
+  (delete-window))
 
 (defun my/disable-key-chord-mode ()
   (set (make-local-variable 'input-method-function) nil))
@@ -390,17 +273,18 @@
   (condition-case nil (next-error)
     (error (next-error 1 t))))
 
-(defun my/delete-window-maybe-kill-buffer ()
-  (interactive)
-  (when (eq 1 (length (get-buffer-window-list)))
-    (kill-this-buffer))
-  (delete-window))
 
-(defun my/create-newline-and-enter-sexp (&rest _ignored)
-  (newline)
-  (indent-according-to-mode)
-  (forward-line -1)
-  (indent-according-to-mode))
-
-
-;;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (go-eldoc company-go yasnippet which-key use-package smooth-scrolling key-seq go-mode fzf expand-region evil-visualstar evil-matchit evil-leader counsel company color-theme-solarized))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
