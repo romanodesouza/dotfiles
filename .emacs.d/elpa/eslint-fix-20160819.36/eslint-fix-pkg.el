72bc5be5eec4c46ce065908a9d054c755079587a
Romano Augusto <romanoaugusto88@gmail.com>
2018-01-03 18:59:42 +0000

    emacs: vendorize elpa

---
+++ .emacs.d/elpa/eslint-fix-20160819.36/eslint-fix-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "eslint-fix" "20160819.36" "Fix JavaScript files using ESLint" 'nil :commit "be90d1e78b1dfd43b6b3b1c06868539e2ac27d3a" :url "https://github.com/codesuki/eslint-fix" :keywords '("javascript" "eslint" "lint" "formatting" "style"))
