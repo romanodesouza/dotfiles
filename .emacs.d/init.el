249f116ff7f40e6d464a49eb5bee0b3aa7791274
Romano de Souza <romanoaugusto88@gmail.com>
2019-05-19 18:24:24 -0300

    emacs: black and white for fzf

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -148,7 +148,7 @@
   :load-path "vendor/fzf.el"
   :commands (fzf fzf-git-grep fzf-rg)
   :init
-  (setq fzf/args "-x -e --print-query")
+  (setq fzf/args "-x -e --print-query --color=bw")
   (setq fzf/git-grep-args "-i --line-number %s -- './*' '!vendor/' '!node_modules/'")
   (setq fzf/rg-args "--trim --hidden -n -S -g '!vendor/' -g '!node_modules/' -g '!.git/' %s"))
 
@@ -342,7 +342,7 @@
 (use-package multiple-cursors
   :commands (mc/mark-next-like-this mc/mark-previous-like-this mc/skip-to-next-like-this)
   :init
-	(define-key evil-visual-state-map (kbd "C-n") 'mc/mark-next-like-this)
+    (define-key evil-visual-state-map (kbd "C-n") 'mc/mark-next-like-this)
     (define-key evil-visual-state-map (kbd "C-p") 'mc/mark-previous-like-this)
     (define-key evil-visual-state-map (kbd "C-s") 'mc/skip-to-next-like-this))
 
