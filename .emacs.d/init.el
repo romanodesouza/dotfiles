5e47912dce1893b6aeb2e7208c16a41689da1ae7
Romano de Souza <romanoaugusto88@gmail.com>
2019-05-19 18:12:39 -0300

    emacs: disable cursor blinking

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -443,6 +443,8 @@
   (add-hook 'emacs-lisp-mode-hook 'remove-elc-on-save)
   ;; Auto recompile if compilation window is visible
   (advice-add 'my/save-buffers :after (lambda () (when (get-buffer "*compilation*") (recompile))))
+  ;; Cursor blinking
+  (blink-cursor-mode 0)
   ;;(add-hook 'prog-mode-hook 'linum-mode)
   (load custom-file))
 
