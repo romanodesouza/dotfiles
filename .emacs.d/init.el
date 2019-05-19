fa56c8d9ec0c2699c56e9808c36107108da6a5d7
Romano de Souza <romanoaugusto88@gmail.com>
2019-05-19 17:40:15 -0300

    emacs: remove marmalade source package

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -84,10 +84,9 @@
 
 ;; Packages
 (require 'package)
-(setq package-archives '(("org"       . "http://orgmode.org/elpa/")
-                         ("gnu"       . "http://elpa.gnu.org/packages/")
-                         ("melpa"     . "http://melpa.org/packages/")
-                         ("marmalade" . "http://marmalade-repo.org/packages/")))
+(setq package-archives '(("org"       . "https://orgmode.org/elpa/")
+                         ("gnu"       . "https://elpa.gnu.org/packages/")
+                         ("melpa"     . "https://melpa.org/packages/")))
 (package-initialize)
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
