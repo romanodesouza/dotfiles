1ea5828905ab92e34ebd2bd288347f53838ffa01
Romano de Souza <romanoaugusto88@gmail.com>
2022-11-24 23:15:42 +0000

    nvim: fix organize imports (sync mode)

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -112,12 +112,32 @@ local on_attach=function(client, bufnr)
 	vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
 end
 
--- lsp format and organize imports
+-- lsp organize imports and buf format
+function sync_org_imports()
+	local clients = vim.lsp.buf_get_clients()
+	for _, client in pairs(clients) do
+
+		local params = vim.lsp.util.make_range_params(nil, client.offset_encoding)
+		params.context = {only = {'source.organizeImports'}}
+
+		local result = vim.lsp.buf_request_sync(0, 'textDocument/codeAction', params, 5000)
+		for _, res in pairs(result or {}) do
+			for _, r in pairs(res.result or {}) do
+				if r.edit then
+					vim.lsp.util.apply_workspace_edit(r.edit, client.offset_encoding)
+				else
+					vim.lsp.buf.execute_command(r.command)
+				end
+			end
+		end
+	end
+end
+
 vim.api.nvim_create_autocmd('BufWritePre', {
 	pattern = {'*.go'},
 	callback = function(args)
+		sync_org_imports()
 		vim.lsp.buf.format({sync=true})
-		vim.lsp.buf.code_action({context={only={'source.organizeImports'}}, apply=true})
 	end,
 })
 
