424619b833f6c88c9c8bafec03543fa1e39f4389
Romano de Souza <romanoaugusto88@gmail.com>
2023-08-27 23:29:24 +0100

    nvim: back with Go support

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -56,7 +56,61 @@ require("lazy").setup({
 	},
 
 	-- Comment stuff out
-	{ "tpope/vim-commentary" }
+	{ "tpope/vim-commentary" },
+
+	-- LSP
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			{ "neovim/nvim-lspconfig" },
+			{ "hrsh7th/cmp-nvim-lsp" },
+			{ "hrsh7th/cmp-vsnip" },
+			{ "hrsh7th/vim-vsnip" },
+			{ "hrsh7th/cmp-nvim-lsp-signature-help" }
+		},
+		config = function()
+			vim.opt.completeopt="menu,menuone,noselect"
+			local cmp = require("cmp")
+
+			cmp.setup({
+				sources = {
+					{ name="nvim_lsp" },
+					{ name="nvim_lsp_signature_help" },
+				},
+				snippet = {
+					-- REQUIRED - you must specify a snippet engine
+					expand = function(args)
+						vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+					end,
+				},
+				mapping = cmp.mapping.preset.insert({
+					["<C-b>"] = cmp.mapping.scroll_docs(-4),
+					["<C-f>"] = cmp.mapping.scroll_docs(4),
+					["<C-Space>"] = cmp.mapping.complete(),
+					["<C-e>"] = cmp.mapping.abort(),
+					["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+				}),
+			})
+
+			local nvim_lsp = require("lspconfig")
+			local on_attach = function(client, bufnr)
+				vim.lsp.handlers["textDocument/hover"]=vim.lsp.with(vim.lsp.handlers.hover, { border="rounded" })
+				local opts = { noremap=true, silent=true, buffer=bufnr }
+				vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+				vim.keymap.set("n", "<C-]>", vim.lsp.buf.definition, opts)
+				vim.keymap.set("n", "<space>i", vim.lsp.buf.implementation, opts)
+				vim.keymap.set("n", "<space>r", vim.lsp.buf.references, opts)
+			end
+
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+			-- Go
+			nvim_lsp.gopls.setup({
+				on_attach=on_attach,
+				capabilities=capabilities
+			})
+		end
+	}
 })
 
 -- clipboard behaviour
@@ -124,3 +178,35 @@ vim.keymap.set({ "x" }, "<leader>a", '"yy:<C-u>Rg <c-r>y<CR>', { silent=true })
 vim.keymap.set({ "n" }, "<leader>a", ":Rg<CR>", { silent=true })
 vim.keymap.set({ "c" }, "<C-g>", "<C-c>", { silent=true })
 vim.keymap.set({ "i", "v", "n" }, "<C-g>", "<ESC>", { silent=true })
+
+
+-- Taken from https://github.com/golang/tools/blob/1f10767725e2be1265bef144f774dc1b59ead6dd/gopls/doc/vim.md#neovim-imports
+function OrgImports(wait_ms)
+	local params = vim.lsp.util.make_range_params()
+	params.context = { only = { "source.organizeImports" } }
+	local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, wait_ms)
+	for _, res in pairs(result or {}) do
+		for _, r in pairs(res.result or {}) do
+			if r.edit then
+				vim.lsp.util.apply_workspace_edit(r.edit, "UTF-8")
+			else
+				vim.lsp.buf.execute_command(r.command)
+			end
+		end
+	end
+end
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+	pattern = { "*.go" },
+	callback = function(args)
+		OrgImports(1000)
+		vim.lsp.buf.format({ sync=true })
+	end
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "go" },
+	callback = function(args)
+		vim.keymap.set({ "n" }, "<leader>d", ":BLines ^type\\|^func <CR>", { silent=true, buffer=true })
+	end
+})
