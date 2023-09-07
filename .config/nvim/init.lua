1100ae836cd02c30844e16db6c46d0d3c4fc12ab
Romano de Souza <romanoaugusto88@gmail.com>
2023-09-07 02:57:54 +0100

    neovim: use Nord theme

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -54,12 +54,13 @@ require("lazy").setup({
 		end
 	},
 	{
-		"projekt0n/github-nvim-theme",
-		lazy = false, -- make sure we load this during startup if it is your main colorscheme
-		priority = 1000, -- make sure to load this before all the other start plugins
+		"shaunsingh/nord.nvim",
+		lazy = false,
+		priority = 1000,
 		config = function()
 			vim.opt.termguicolors=true
-			vim.cmd.colorscheme("github_dark_dimmed")
+			vim.g.nord_bold=false
+			vim.cmd.colorscheme("nord")
 		end
 	},
 
